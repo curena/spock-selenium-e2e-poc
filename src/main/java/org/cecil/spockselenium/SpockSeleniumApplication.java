2ace92b2cc70e99a5d72c293f98bcf1df8da4238
Cecil Urena <cecil.urena@gmail.com>
2022-07-17 13:48:53 -0500

    First project commit

---
+++ src/main/java/org/cecil/spockselenium/SpockSeleniumApplication.java
@@ -0,0 +1,13 @@
+package org.cecil.spockselenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockSeleniumApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpockSeleniumApplication.class, args);
+  }
+
+}
